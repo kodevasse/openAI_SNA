de43f23a8a04137f44fe6dab73b7d99e0d315057
kodevasse <kodevasse@outlook.com>
2022-12-11 00:25:11 +0100

    small launch for SNA

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,15 +1,48 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+const generateText = async (req, res) => {
+  const { inputText } = req.body;
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: inputText,
+      temperature: 0.5,
+      max_tokens: 60,
+      top_p: 0.3,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+    });
+
+    const textResponse = response.data.choices[0].text;
+
+    res.status(200).json({
+      success: true,
+      data: textResponse,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: "There was an error fetching your question",
+    });
+  }
+};
 const generateImage = async (req, res) => {
   const { prompt, size } = req.body;
 
   const imageSize =
-    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+    size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
 
   try {
     const response = await openai.createImage({
@@ -34,9 +67,9 @@ const generateImage = async (req, res) => {
 
     res.status(400).json({
       success: false,
-      error: 'The image could not be generated',
+      error: "The image could not be generated",
     });
   }
 };
 
-module.exports = { generateImage };
+module.exports = { generateImage, generateText };
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const { generateImage } = require('../controllers/openaiController');
+const express = require("express");
+const {
+  generateImage,
+  generateText,
+} = require("../controllers/openaiController");
 const router = express.Router();
 
-router.post('/generateimage', generateImage);
+router.post("/generateimage", generateImage);
+router.post("/generatetext", generateText);
 
 module.exports = router;
