046cb1ed39a1b41b0e42ec8d52ec0dd2e36e3135
kodevasse <kodevasse@outlook.com>
2022-12-10 22:10:40 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public/dist")));
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
