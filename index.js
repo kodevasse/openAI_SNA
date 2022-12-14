e55b302462a4e236aa634cfe808ea219313fc205
kodevasse <kodevasse@outlook.com>
2022-12-14 07:05:55 +0100

    removed cors

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 // allow cors localhost
-const allowedOrigins = ["https://localhost:5173"];
-// Enable body parser
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// const allowedOrigins = ["https://localhost:5000"];
+// // Enable body parser
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
